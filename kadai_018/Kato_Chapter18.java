77f880e6c00f482e5989663362ab839c2c6a54a8
kimu-y7 <y.kimura@e-corporation.co.jp>
2026-05-26 13:44:26 +0900

    継承クラス課題

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,26 @@
+package kadai_018;
+
+abstract public class Kato_Chapter18 {
+	//姓を表す
+	public String familyName = "加藤";
+	//名を表す
+	public String givenName = "";
+	//住所を表す
+	public String address = "東京都中野区〇×";
+	
+	//共通の紹介を出力するメソッド
+	public void commonIntroduce () {
+		System.out.println("名前は" + familyName + givenName +"です");
+		System.out.println("住所は" + address + "です");
+	}
+	
+	//個別の紹介を出力する抽象メソッド
+	abstract public void eachIntroduce ();
+	
+	//紹介を実行するメソッド
+	public void execIntroduce () {
+		commonIntroduce();
+		eachIntroduce ();
+	}
+	
+}
